ac64904f903bed452979490abe13d1ca07c7afe9
ALbert Chen <albert.c.x.chen@oocl.com>
2021-06-16 21:44:16 +0800

    add unit test for process by mock

--- cash-register-2021-6-14-13-47-19-956-master/src/test/java/com/tw/academy/CashRegisterTest.java
+++ cash-register-2021-6-14-13-47-19-956-master/src/test/java/com/tw/academy/CashRegisterTest.java
@@ -8,6 +8,32 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class CashRegisterTest {
+	@Test
+	void should_call_print_when_process_given_purchase() {
+		//given
+		Purchase purchase = new Purchase();
+		Printer  print= mock (Printer.class);
+		CashRegister cashRegister = new CashRegister(print);
+		//when
+		cashRegister.process(purchase);
+		//then
+		//verify that cashRegister.process will trigger print
+		verify(print,times(1)).print(anyString());
+	}
+	@Test
+	void should_print_purchase_when_process_given_purchase() {
+		//given
+		Purchase purchase = mock(Purchase.class);
+		Printer  print= mock (Printer.class);
+		CashRegister cashRegister = new CashRegister(print);
+		//when
+		when(purchase.asString()).thenReturn("123");
+		cashRegister.process(purchase);
+
+		//then
+		//verify that cashRegister.process will trigger print
+		verify(print).print("123");
+	}
 
 
 }
