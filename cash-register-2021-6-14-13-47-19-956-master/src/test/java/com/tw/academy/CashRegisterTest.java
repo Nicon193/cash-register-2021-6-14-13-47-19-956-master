7a8f9c0fe2d851c55d26bdb83c6637424a47c3d9
ALbert Chen <albert.c.x.chen@oocl.com>
2021-06-16 21:40:26 +0800

    git init

---
+++ cash-register-2021-6-14-13-47-19-956-master/src/test/java/com/tw/academy/CashRegisterTest.java
@@ -0,0 +1,13 @@
+package com.tw.academy;
+
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Test;
+import sun.security.x509.OtherName;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+
+class CashRegisterTest {
+
+
+}
